d8524a6764c49988ef0245d3d4aad19254299322
jens5962 <jensrand1999@gmail.com>
2022-06-05 17:21:16 +0200

    Scraper working

---
+++ README.txt
@@ -0,0 +1,5 @@
+VIGTIGT for at web scraper(guzzle_requests.php) virker:
+- installeret nyeste version af "Composer" 
+
+Derefter skal Guzzle installeres ved at skrive følgende i din terminal:
+composer require guzzlehttp/guzzle
--- guzzle_requests.php
+++ guzzle_requests.php
@@ -1,8 +1,8 @@
 <?php
-# scraping blog posts: https://www.pinterest.dk/beachbrights/ikea-hacks/
+# scraping blog posts: https://www.housebeautiful.com/home-remodeling/diy-projects/g2826/best-ikea-hacks/
 require 'vendor/autoload.php';
 $httpClient = new \GuzzleHttp\Client();
-$response = $httpClient->get('https://www.pinterest.dk/beachbrights/ikea-hacks/');
+$response = $httpClient->get('https://www.housebeautiful.com/home-remodeling/diy-projects/g2826/best-ikea-hacks/');
 $htmlString = (string) $response->getBody();
 //supresses warnings
 libxml_use_internal_errors(true);
@@ -11,15 +11,16 @@ $doc->loadHTML($htmlString);
 $xpath = new DOMXPath($doc);
 
 
-//Targeting content inside "<img"
-$titles = $xpath->evaluate('//*[@id="boardfeed:20266335762128165"]/div/div[1]/div[2]/div/div/div/div/div[1]/a/div/div/div/div/div[1]/img');
+//Targeting content inside "span" as it's the headline on every section
+//Vi har hele stien op til hvor overteksterne starter der efter bruger vi "//" inden "span" for at 
+//få ALLE dem som står under "span" med(alle overskrifterne)  
+$titles = $xpath->evaluate('/html/body/main/div/div/div//span');
+
 $extractedTitles = [];
 //We use a foreach loop to extract the text content and echo them to the terminal  
+//man kan vælge med xpath hvad det er man søger efter derfor siger vi her at vi vil have tekst.
 foreach ($titles as $title) {
 $extractedTitles[] = $title->textContent.PHP_EOL;
 echo $title->textContent.PHP_EOL;
 }
-
-
-
 ?>
